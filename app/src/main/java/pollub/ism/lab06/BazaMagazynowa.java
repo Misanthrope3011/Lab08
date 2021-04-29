d7c8e549bd51d8788bb713d406d6d7c2999cf2ae
Misanthrope <carrion.30.11@gmail.com>
2021-04-29 19:18:28 +0200

    toast

---
+++ app/src/main/java/pollub/ism/lab06/BazaMagazynowa.java
@@ -0,0 +1,15 @@
+package pollub.ism.lab06;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+@Database(entities = {PozycjaMagazynowa.class, HistoriaTransakcji.class}, version = 2, exportSchema = false)
+public abstract class BazaMagazynowa extends RoomDatabase {
+
+    public static final String NAZWA_BAZY = "Warzywniak1";
+    public abstract PozycjaMagazynowaDAO pozycjaMagazynowaDAO();
+    public abstract Transackcje transackcje();
+}
